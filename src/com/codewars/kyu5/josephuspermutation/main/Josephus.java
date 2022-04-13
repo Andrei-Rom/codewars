776bd680451bc552cef01c64b00abcb9c9e23082
Andrei <andrisrom@gmail.com>
2022-04-13 12:55:43 +0300

    change name of variable

--- src/com/codewars/kyu5/josephuspermutation/main/Josephus.java
+++ src/com/codewars/kyu5/josephuspermutation/main/Josephus.java
@@ -7,10 +7,11 @@ public class Josephus {
     // 1, 2, 3, 4, 5, 6, 7}, 3 -> {3, 6, 2, 7, 5, 1, 4}
     public static <T> List<T> josephusPermutation(final List<T> items, final int k) {
         ArrayList result = new ArrayList();
-        int m = items.size();
+        int size = items.size();
         int step = k - 1;
         int j = 0;
-        for (int i = 0; i < m; i++) {
+
+        for (int i = 0; i < size; i++) {
             j += step;
             while (j > items.size() - 1) {
                 j -= items.size();
